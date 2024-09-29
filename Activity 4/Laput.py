ecf98eab93222e59e7b89817d4839776932148df
jlsl444 <laputjeyrald@gmail.com>
2024-09-29 23:46:44 +0800

    Create Laput.py

---
+++ Activity 4/Laput.py
@@ -0,0 +1,30 @@
+# sampleText1 = "My name is {}. I love {}. I love playing {}. {}"
+# sampleText1a = sampleText1.format("Jeyrald", "Sinigang", "Clash Of Clans", "Pineapple")
+# print(sampleText1a)
+
+
+
+# sampleText2 = "My name is {1}. I love {2}. I love playing {0}."
+# sampleText2a = sampleText2.format("NBA", "Jeyrald", "Sinigang", "Clash Of Clans", "Pineapple")
+# print(sampleText2a)
+
+
+
+# sampleText3 = "My name is {name}. I love {food}. I love playing {play}."
+# sampleText3a = sampleText3.format(food="Sinigang", play="Clash Of Clans", name="Jeyrald")
+# print(sampleText3a)
+
+
+
+
+
+# item = "Pancit Canton"
+# cost = 14.5
+# SampleText4 = "The product is %s the cost is %.5f." % (item, cost)
+# print(SampleText4) 
+
+item = "Nike Air force"
+cost = 12000
+
+Sample Text5 = f"The item is {item} and the cost is {cost * 12000} pesos."
+print(SampleText5)
