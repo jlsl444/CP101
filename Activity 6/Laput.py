01447516f0355b023ff74335c4e60d33dd5ea69b
jlsl444 <laputjeyrald@gmail.com>
2024-10-03 16:30:33 +0800

    Create Laput.py

---
+++ Activity 6/Laput.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f"Hello, {name}! You are {age} years old.")
+
+item = "Shoes"
+count = 1
+sentence = "I bought {} {} today.".format(count, item)
+print(sentence)
+
+city = "Dipolog"
+temperature = 40
+print("The temperature in %s is %d degrees Celsius." % (city, temperature)
